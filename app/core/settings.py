ba46c135ffda0b2efc2ddb23312b468bd42e6fcd
Falco90 <falcorodenburg@gmail.com>
2026-01-22 16:08:32 +0700

    authorize Github, get access token and fetch user data

---
+++ app/core/settings.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+class Settings(BaseSettings):
+    #app
+    APP_NAME:str = "Commico"
+    DEBUG:bool = False
+
+    #OAuth
+    GITHUB_CLIENT_ID: str | None = None
+    GITHUB_CLIENT_SECRET: str | None = None
+    GITHUB_REDIRECT_URI: str | None = None
+
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_file_encoding="utf-8"
+    )
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,152 +1,75 @@
 from typing import Annotated
 from datetime import datetime, timedelta, timezone
+import httpx
 
 import jwt
 from jwt.exceptions import InvalidTokenError
 from fastapi import Depends, FastAPI, HTTPException, Security
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, SecurityScopes
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel, ValidationError
 from pwdlib import PasswordHash
-
-SECRET_KEY="90ba19a9278a7dc7a6ade73eb69d81f75a40501f9ed2f0c65acf8deccf5dc984"
-ALGORITHM="HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES=30
-
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$wagCPXjifgvUFBzq4hqe3w$CYaIb8sB+wtD+Vu/P4uod1+Qof8h+1g7bbDlBID48Rc",
-        "disabled": False,
-    }
-}
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-class TokenData(BaseModel):
-    username: str | None = None
-    scopes: list[str] = []
+from core.settings import settings
 
 app = FastAPI()
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", scopes={"me": "Read information about the current user.", "items": "Read items."},
-)
-
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-
-class UserInDB(User):
-    hashed_password: str
-
-password_hash = PasswordHash.recommended()
-
-def verify_password(plain_password, hashed_password):
-    return password_hash.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return password_hash.hash(password)
-
-def get_user(db, username):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-def fake_decode_token(token):
-    user = get_user(fake_users_db, token)
-    return user
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)]):
-    if security_scopes.scopes:
-        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
-    else:
-        authenticate_value = "Bearer"
-    credentials_exception = HTTPException(
-            status_code=401,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": authenticate_value},
+@app.get("/auth/github/login")
+async def github_login():
+    url = (
+        "https://github.com/login/oauth/authorize"
+        f"?client_id={settings.GITHUB_CLIENT_ID}"
+        f"&redirect_uri={settings.GITHUB_REDIRECT_URI}"
+        "&scope=read:user user:email"
     )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        scope: str = payload.get("scope", "")
-        token_scopes = scope.split(" ")
-        token_data = TokenData(scopes=token_scopes, username=username)
-    except (InvalidTokenError, ValidationError):
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception        
-    for scope in security_scopes.scopes:
-        if scope not in token_data.scopes:
-            raise HTTPException(
-                status_code=401,
-                detail="Not enough permissions",
-                headers={"WWW-Authenticate": authenticate_value},
-            )
-    return user
+    return RedirectResponse(url)
 
-async def get_current_active_user(current_user: Annotated[User, Security(get_current_user, scopes=["me"])]):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="inactive user")
-    return current_user
+@app.get("/auth/github/callback")
+async def github_callback(code: str):
+    token_url = "https://github.com/login/oauth/access_token"
+    
+    async with httpx.AsyncClient() as client:
+        token_response = await client.post(
+            token_url,
+            headers={"Accept": "application/json"},
+            data={
+                "client_id": settings.GITHUB_CLIENT_ID,
+                "client_secret": settings.GITHUB_CLIENT_SECRET,
+                "code": code,
+                "redirect_uri": settings.GITHUB_REDIRECT_URI,
+            },
+        )
 
-@app.post("/token")
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=401, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.username, "scope": " ".join(form_data.scopes)}, expires_delta=access_token_expires)
+    if token_response.status_code != 200:
+        raise HTTPException(status_code=400, detail="Github token exchange failed")
 
-    return Token(access_token=access_token, token_type="bearer")
+    token_data = token_response.json()
+    print(f"token_data: {token_data}")
+    access_token = token_data.get("access_token")
 
+    if not access_token:
+        raise HTTPException(status_code=400, detail="No access token from Github")
 
-@app.get("/users/me", response_model=User)
-async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
-    return current_user
+    async with httpx.AsyncClient() as client:
+        user_response = await client.get(
+            "https://api.github.com/user",
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Accept": "application/vnd.github+json",
+            },
+        )
+        
+        if user_response.status_code != 200:
+            raise HTTPException(status_code=400, detail="Could not fetch Github user")
 
-@app.get("/users/me/items")
-async def read_own_items(current_user: Annotated[User, Security(get_current_active_user, scopes=["items"])]):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
-
-@app.get("/status/")
-async def read_system_status(current_user: Annotated[User, Depends(get_current_user)]):
-    return {"status": "ok"}
-
-
+        github_user = user_response.json()
+        print(f"response: {github_user}")
+        github_id = github_user["id"]
+        github_username = github_user["login"]
+        email = github_user.get("email")
+        
+        print(f"github id: {github_id}")
+        print(f"github username: {github_username}")
+        print(f"github email: {email}")
 
 
 
