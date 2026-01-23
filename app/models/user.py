529662b32f6f9e7173fb2e3d23f29d72042acc71
Falco90 <falcorodenburg@gmail.com>
2026-01-23 21:14:00 +0700

    add User model

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlmodel import Field, SQLModel, create_engine
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    github_id: int
+    github_username: str
+    email: str
+
+
+
+
+
