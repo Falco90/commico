43fbfdf6145f76441f8a44bbb95ae54059c61460
Falco90 <falcorodenburg@gmail.com>
2026-01-30 22:00:40 +0700

    move jwt encryption logic to core/security folder

--- app/core/security/encryption.py
+++ app/core/security/encryption.py
@@ -1,5 +1,8 @@
 from cryptography.fernet import Fernet
 from app.core.settings import settings
+from datetime import datetime, timedelta, timezone
+from typing import Any
+import jwt
 
 fernet = Fernet(settings.GITHUB_TOKEN_ENCRYPTION_KEY)
 
@@ -8,3 +11,32 @@ def encrypt_github_token(token: str) -> str:
 
 def decrypt_github_token(token_encrypted: str) -> str:
     return fernet.decrypt(token_encrypted.encode()).decode()
+
+def create_access_token(
+    data: dict[str, Any],
+    expires_delta: timedelta | None = None
+) -> str:
+    to_encode = data.copy()
+
+    now = datetime.now(timezone.utc)
+    if expires_delta:
+        expire = now + expires_delta
+    else:
+        expire = now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update(
+        {
+            "exp": expire,
+            "iat": now,
+            "type": "access"
+        }
+    )
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
+
